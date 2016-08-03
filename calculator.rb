68745746f482e8f72aeaa92275c9dc05b18d617d
Alex Quan <alxquan@gmail.com>
2016-08-03 16:29:09 -0600

    looped through once result was calculated

--- calculator.rb
+++ calculator.rb
@@ -13,21 +13,40 @@ def operator(num_1, num_2)
 		num_1 * num_2
 		#multiplication operation
 	when "clear"
-		#clear operation
+		puts "Clearing numbers from memory"
 	else
 		puts "#{@modifier} is not a valid input. Please select from (+ - / *)"
 	end
 end
 
-puts "Please input first number"
-num_1 = gets.to_f
-puts "Please enter an operation (+ - / *)"
-@modifier = gets.chomp
-puts "Please input second number"
-num_2 = gets.to_f
+solution = nil
+while true
+  if solution
+    # tell the user that they have a result
+    # ask the user what they want to do: use result, clear, quit
+    # if quit, exit
+    # if clear, nil out solution variable and ask for the first number again
+    # if use result continue
+    num_1 = solution
+  else 
+    puts "Please input first number or type quit"
+    num_1 = gets.chomp.downcase
+      if num_1 == "quit"
+        exit 0 
+      else
+        num_1 = num_1.to_f
+      end
+  end
+    puts "Please enter an operation (+ - / *)"
+    @modifier = gets.chomp
+    puts "Please input second number"
+    num_2 = gets.to_f
 
-puts "#{num_1} #{@modifier} #{num_2} is equal to #{operator(num_1, num_2)}"
- 
+  puts "#{num_1} #{@modifier} #{num_2} is equal to #{operator(num_1, num_2)}"
+
+  solution = operator(num_1, num_2) 
+end
+
 #result of answer should continue to build until user clears calculator
 
 #store result as new num1, second gets input as num2
